310e2eb9694a5e640a0e0279f2ec33e3284e0638
Adrian Metzler <47183489+aametzler@users.noreply.github.com>
2020-01-21 13:05:01 -0400

    fixed description column sorting for history page (#27)
    * fixed current tickets date filter to match dashboard filter
    
    * fixed description column sorting for history page

--- src/components/Dashboard/Sweeps/CurrentTickets.jsx
+++ src/components/Dashboard/Sweeps/CurrentTickets.jsx
@@ -73,17 +73,17 @@ function mapStateToProps(state) {
             }
           }
             break;
-          case 'dateRange':
-            let sDate = f.val.start_date,
-              eDate = f.val.end_date;
-  
-            passed =
+            case 'dateRange':
+              let sDate = f.val.start_date,
+                eDate = f.val.end_date;
+
+              passed =
               passed &&
               ((eDate === undefined ||
-                moment(lottoEnd).isBefore(eDate + ' 23:59:59')) &&
+                moment(lottoEnd).isBefore(`${eDate } 23:59:59`)) &&
                 (sDate === undefined || moment(lottoEnd).isAfter(sDate)));
-  
-            break;
+
+              break;
         }
       });
       return passed;
